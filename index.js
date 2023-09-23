5a76d229c6512e7c8be6f5f864e6e7e76dd24081
Elicarlos <elicarlosantos_@hotmail.com>
2023-09-23 17:12:15 -0300

    ajustes

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
 const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const axios = require('axios');
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html'); // Corrigi o caminho para o arquivo index.html
+// Middleware para permitir solicitações CORS (se necessário)
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Altere '*' para permitir somente origens específicas
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
 });
 
-// Rota para incorporar a página 'pagina.html' usando um iframe
+// Rota para carregar a página de forma segura usando HTTPS
+app.get('/load-page', async (req, res) => {
+  try {
+    // Use Axios para buscar o conteúdo da página com HTTPS
+    const response = await axios.get('https://admin.tabletcloud.com.br/login.aspx?ReturnUrl=/newcloud/uwg');
+
+    // Envie o conteúdo da página como resposta
+    res.send(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Erro ao carregar a página');
+  }
+});
+
+// Rota para servir a página 'pagina.html' usando um iframe
 app.get('/embed', (req, res) => {
-    res.sendFile(__dirname + '/public/pagina.html'); // Especifique o caminho correto para a página 'pagina.html'
+  res.sendFile(__dirname + '/public/pagina.html'); // Especifique o caminho correto para a página 'pagina.html'
 });
 
-app.listen(port, () =>{
-    console.log('Servidor ok');
+// Rota para a página inicial
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html'); // Corrigi o caminho para o arquivo index.html
+});
+
+// Inicia o servidor
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
 });
