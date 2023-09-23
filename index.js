e62b77917996ecfb0bef8db267a845cb1f469ee5
Elicarlos <elicarlosantos_@hotmail.com>
2023-09-23 16:58:26 -0300

    ajustes

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const app = express()
-const port = 3000
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + 'public/index.html')
-})
+    res.sendFile(__dirname + '/public/index.html'); // Corrigi o caminho para o arquivo index.html
+});
+
+// Rota para incorporar a página 'pagina.html' usando um iframe
+app.get('/embed', (req, res) => {
+    res.sendFile(__dirname + '/public/pagina.html'); // Especifique o caminho correto para a página 'pagina.html'
+});
 
 app.listen(port, () =>{
-    console.log('Servidor ok')
-})
-
-module.exports = app
-
-
+    console.log('Servidor ok');
+});
